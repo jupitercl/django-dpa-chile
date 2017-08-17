2c37b00683a7bd4b38c533914ddc2a2752f04029
Francisco Jordan <francisco@qin.cl>
2017-08-16 22:27:20 -0300

    modifications to pypi

--- README.rst
+++ README.rst
@@ -1,8 +1,8 @@
 # django-dpa-chile
 
-[![Build Status][travis-image]][travis-url]
+<!-- [![Build Status][travis-image]][travis-url]
 [![Coverage percentage][coveralls-image]][coveralls-url]
-[![PyPI status][pypi-image]][pypi-url]
+[![PyPI status][pypi-image]][pypi-url] -->
 
 > Political-Administrative Division of Chile
 
@@ -33,9 +33,9 @@ INSTALLED_APPS =[
 
 Francisco Jordan &lt;franciscojordan@live.com&gt;
 
-[travis-image]: https://travis-ci.org/jupitercl/django-dpa-chile.svg?branch=master
+<!-- [travis-image]: https://travis-ci.org/jupitercl/django-dpa-chile.svg?branch=master
 [travis-url]: https://travis-ci.org/jupitercl/django-dpa-chile
 [coveralls-image]: https://coveralls.io/repos/jupitercl/django-dpa-chile/badge.svg
 [coveralls-url]: https://coveralls.io/r/jupitercl/django-dpa-chile
-[pypi-image]: https://img.shields.io/pypi/v/django-dpa-chile.svg
+[pypi-image]: https://img.shields.io/pypi/v/django-dpa-chile.svg -->
 [pypi-url]: https://pypi.python.org/pypi/django-dpa-chile/
--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python
-from setuptools import setup
+
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-dpa-chile',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.2.1',
+
+    description='Political-Administrative Division of Chile',
+    long_description=long_description,
+
+    # The project's main homepage.
     url='https://github.com/jupitercl/django-dpa-chile',
+
+    # Author details
     author='Francisco Jordan',
     author_email='franciscojordan@live.com',
-    description=('Political-Administrative Division of Chile'),
-    long_description=open('README.md').read(),
-    keywords='django,chile,comunas,regiones,provincias',
-    license=open('LICENSE').read(),
-    platforms=['Mac'],
-    packages=[
-        'django_dpa_chile',
-        'django_dpa_chile.migrations',
-        'django_dpa_chile.management',
-        'django_dpa_chile.management.commands'],
-    include_package_data=True,
-    install_requires=[
-        'django>=1.11',
-        'urllib3',
-        'bunch'
-    ],
-    extras_require={},
-    # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -36,5 +43,28 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Other/Nonlisted Topic'],
+    ],
+
+    # What does your project relate to?
+    keywords='django chile comunas regiones provincias',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=[
+        'django_dpa_chile',
+        'django_dpa_chile.migrations',
+        'django_dpa_chile.management',
+        'django_dpa_chile.management.commands'],
+
+    include_package_data=True,
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'django>=1.11',
+        'urllib3',
+        'bunch'
+    ],
 )
